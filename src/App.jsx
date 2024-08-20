2b23e161acd54ca5bde590853343cf857eabd70b
Ethan Farber <edf1247@gmail.com>
2024-08-19 23:02:01 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
+  const course = {
+    name: 'Half Stack application development',
+
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+}
+
 
   return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+const Header = ({course}) => {
+  
+  return (
+  <>
+    <h1>{course}</h1>
+  </>)
+}
+
+const Content = ({parts}) => {
+
+  let part1 = parts[0];
+  let part2 = parts[1];
+  let part3 = parts[2];
+  
+  return (<div>
+      <Part name={part1.name} exercise={part1.exercises} />
+      <Part name={part2.name} exercise={part2.exercises} />
+      <Part name={part3.name} exercise={part3.exercises} />
+  </div>)
+}
+
+const Part = ({name, exercise}) => {
+  return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        {name} {exercise}
       </p>
     </>
-  )
+  );
+}
+
+const Total = ({parts}) => {
+  return (<>
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  </>);
 }
 
 export default App
